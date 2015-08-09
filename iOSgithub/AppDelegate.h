d5672dec885fb457784e71a29cb78741be94a57c
Anastasya <Nastya@Anastasyas-MacBook-Pro.local>
2015-08-09 16:46:31 +0300

    Initial Commit

---
+++ iOSgithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSgithub
+//
+//  Created by Anastasya on 09.08.15.
+//  Copyright (c) 2015 PeaceData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
